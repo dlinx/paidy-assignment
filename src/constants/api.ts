6e04e0c741e6f6235b0a208d8355dc818e2838c7
Dange Laxmikant <5583697+dlinx@users.noreply.github.com>
2022-02-27 20:51:58 +0900

    fixed api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
 export const API_SERVER = `http://localhost:8080`;
-export const API_URL = `/streaming/ratesa`;
+export const API_URL = `/streaming/rates`;
 export const AUTH_TOKEN = `10dc303535874aeccc86a8251e6992f5`;
